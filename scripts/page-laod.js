48c8aa31021b15ebeaf54da61f2695413c6a4035
Fa-Dream <137039234+Fa-Dream@users.noreply.github.com>
2023-11-03 21:07:32 +0800

    小更新
    取消侧栏选择时可能会出现的文本选择，影响感官。

--- scripts/page-laod.js
+++
@@ -1,5 +0,0 @@
-const iframe = document.getElementById("ifm");
-iframe.onload = function() {
-    let content = iframe.contentWindow.document;
-    content.querySelector("body").style.backgroundImage = "images/test.jpg";
-}

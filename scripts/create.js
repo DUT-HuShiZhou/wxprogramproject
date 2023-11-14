d09d7faf8e9b648413e6adc717d1b130b879a378
Fa-Dream <137039234+Fa-Dream@users.noreply.github.com>
2023-11-14 21:48:38 +0800

    更新

--- scripts/create.js
+++ scripts/create.js
@@ -2,6 +2,6 @@ let sl = document.querySelector("button.sl-btn");
 
 sl.onclick = function() {
     let ifm = document.querySelector("iframe.line-ifm");
-
-    ifm.src = "drama-line.html?num=7";
+    
+    ifm.src = "drama-line.html?num=50 ";
 };
--- scripts/drama-line.js
+++ scripts/drama-line.js
@@ -6,26 +6,30 @@ document.addEventListener('DOMContentLoaded', function() {
     var grades = [];
 
     function displayGrades() {
-      var table = document.getElementById('gradesTable');
-      table.innerHTML = '';
-      var headerRow = table.insertRow();
-      var courseHeader = headerRow.insertCell();
-      courseHeader.innerHTML = '<b>线路名称</b>';
-      var gradeHeader = headerRow.insertCell();
-      gradeHeader.innerHTML = '<b>操作</b>';
-      for (var i = 0; i < grades.length; i++) {
-        var row = table.insertRow();
-        var courseCell = row.insertCell();
-        courseCell.innerHTML = grades[i].course;
-        var gradeCell = row.insertCell();
-        gradeCell.innerHTML = grades[i].grade;
-      }
-    }
+        var table = document.getElementById('gradesTable');
+        table.innerHTML = '';
+        var headerRow = table.insertRow();
+        var orderHeader = headerRow.insertCell();
+        orderHeader.innerHTML = '<b>序号</b>'
+        var courseHeader = headerRow.insertCell();
+        courseHeader.innerHTML = '<b>线路名称</b>';
+        var gradeHeader = headerRow.insertCell();
+        gradeHeader.innerHTML = '<b>操作</b>';
+        for (var i = 0; i < grades.length; i++) {
+            var row = table.insertRow();
+            var orderCell = row.insertCell();
+            orderCell.innerHTML = grades[i].order;
+            var courseCell = row.insertCell();
+            courseCell.innerHTML = grades[i].course;
+            var gradeCell = row.insertCell();
+            gradeCell.innerHTML = grades[i].grade;
+        }
+        }
 
     var course = 0;
     var grade = 0;
     for (var j = 0; j < num; j++)
-    grades.push({ course: course, grade: grade });
+        grades.push({order: j + 1, course: course, grade: grade });
     // 刷新表格
     displayGrades();
-  });
+    });

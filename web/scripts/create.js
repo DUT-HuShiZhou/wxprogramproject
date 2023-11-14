d195b2dc6b227e05df7eeb6be6acef64e2bb16a8
Fa-Dream <137039234+Fa-Dream@users.noreply.github.com>
2023-11-15 01:09:16 +0800

    更新

--- web/scripts/create.js
+++ web/scripts/create.js
@@ -1,4 +1,7 @@
 let sl = document.querySelector("button.sl-btn");
+let fw = document.querySelector("button.fw-btn");
+
+let container = document.querySelector("nav.container");
 
 sl.onclick = function() {
     let ifm = document.querySelector("iframe.line-ifm");
@@ -14,5 +17,24 @@ sl.onclick = function() {
         ifm.style.backgroundColor = "white";
         ls.style.display = "block";
     }
+};
 
+fw.onclick - function() {
+    var xhr = new XMLHttpRequest();
+    var url = "the post url";
+    var params = "type=formworknum"
+    xhr.open("POST", url, true);
+
+    //xhr.setRequestHeader(); 设置请求头，如果有需要
+    xhr.onreadystatechange = function() {
+        var data = xhr.responseText;
+        for (var i = 0; i < data; i++) {
+            var button = document.createElement("button");
+            button.textContent = "模板" + (i + 1);
+            button.id = "num" + (i + 1);
+            container.appendChild(button);
+        }
+    }
+
+    xhr.send(params)
 };
